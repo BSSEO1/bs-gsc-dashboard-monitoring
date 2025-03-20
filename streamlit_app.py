70d4934341d9f00ac9cba2c1341252a986c7650c
Riswanda <riswandaa@Riswandas-MacBook-Air.local>
2025-03-20 15:46:07 +0700

    update client secret

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,7 +50,7 @@ with tab1:
     st.write("")
 
     # Convert secrets from the TOML file to strings
-    clientSecret = str(st.secrets["installed"]["client_email"])
+    clientSecret = str(st.secrets["installed"]["client_secret"])
     clientId = str(st.secrets["installed"]["client_id"])
     redirectUri = str(st.secrets["installed"]["redirect_uris"][0])
 
